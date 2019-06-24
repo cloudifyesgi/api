ee66267693ad8f7627e7f8f59d758d0ed14a58e9
Luis Valdez <valdez.jds.16@gmail.com>
2019-06-24 22:15:04 +0200

    [FIXED] user auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,5 @@
 'use strict';
+const mongoose = require('mongoose');
 
 const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
@@ -12,9 +13,9 @@ module.exports = function(passport) {
         secretOrKey: process.env.PASSPORT_SECRET
     };
     passport.use('jwt', new JwtStrategy(opts, function(jwt_payload, done) {
-        User.findOne(jwt_payload.id)
+        User.findOne(mongoose.Types.ObjectId(jwt_payload._id))
             .then((user) => { return done(null, user); })
-            .catch((error) => { return done(error, false); });
+            .catch((error) => {return done(error, false); });
     }));
 };
 
